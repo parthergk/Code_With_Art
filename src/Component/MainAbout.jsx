293ce5fafb7044b08553b251c33378f4498270a0
Gaurav Kumar <129961702+gaurav5xy@users.noreply.github.com>
2024-06-02 22:36:37 +0530

    Update MainAbout.jsx

--- src/Component/MainAbout.jsx
+++ src/Component/MainAbout.jsx
@@ -8,18 +8,14 @@ const MainAbout = () => {
         <div className=" font-medium text-xs font-roboto">Who Am i? / README <span className=" text-gray-500">.me</span></div>
         <h1 className=" my-2 font-semibold text-3xl font-roboto">Hello!</h1>
         <div className=" my-3 h-px w-full mt-1 bg-gradient-to-r from-gray-300/10 via-gray-500/100 to-gray-300/10"></div>
-        <p className=" w-full ">I am Gaurav Kumar. I am a dedicated and innovative Frontend Developer with a passion for crafting engaging and user-friendly web experiences. With a strong background in HTML, CSS, Bootstrap, JavaScript, Tailwind CSS, React.js, and Redux, I specialize in developing dynamic dashboards, interactive applications, and visually appealing interfaces.
-
-Achievements:
-
-Successfully deployed a chat application using React.js and Node.js with Socket.IO on Vercel.
-
-Developed a video chat app enabling real-time interaction for up to four participants with integrated quiz features.
-
-Created a portfolio showcasing projects developed with React.js, Tailwind CSS, Framer Motion, and Redux.
-
-Completed a Bachelor of Computer Applications (BCA) and applied for internships in the algorithmic domain and website design revamp.
-I am committed to leveraging my skills to create seamless, intuitive, and aesthetically pleasing digital solutions that enhance user engagement and satisfaction.</p>
+        <p className=" w-full ">I am Gaurav Kumar. I am a dedicated and innovative Frontend Developer with a passion for crafting engaging and user-friendly web experiences. With a strong background in HTML, CSS, Bootstrap, JavaScript, Tailwind CSS, React.js, and Redux, I specialize in developing dynamic dashboards, interactive applications, and visually appealing interfaces.
+        <br/><br/>Completed Bachelor of Computer Applications (BCA), focusing on algorithm design and web development. Committed to creating seamless, intuitive, and visually appealing digital solutions to enhance user engagement and satisfaction.</p>
+        <br/>Successfully developed and deployed various web applications, including:<br/>
+        <br/><strong>1. Teacher Review System:</strong> An online platform for students to provide feedback on their teachers' performance.
+        <br/><strong>2. GoAssignr:</strong> A task management system designed to streamline assignment and project tracking.
+        <br/><strong>3. ChatterRoom:</strong> A chat application using React.js and Node.js with Socket.IO, deployed on Vercel.
+        <br/><strong>4. CodeWithArt:</strong> A portfolio showcasing projects developed with React.js, Tailwind CSS, Framer Motion, and Redux.
+        
       </div>
     )
   }
