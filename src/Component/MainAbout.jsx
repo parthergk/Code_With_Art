34ac58f0c9fe09a0b1c78f02ef964ac073f221e1
gaurav5xy <gauravkumar81464@gmail.com>
2024-06-02 22:34:34 +0530

    new

--- src/Component/MainAbout.jsx
+++ src/Component/MainAbout.jsx
@@ -8,7 +8,16 @@ const MainAbout = () => {
         <div className=" font-medium text-xs font-roboto">Who Am i? / README <span className=" text-gray-500">.me</span></div>
         <h1 className=" my-2 font-semibold text-3xl font-roboto">Hello!</h1>
         <div className=" my-3 h-px w-full mt-1 bg-gradient-to-r from-gray-300/10 via-gray-500/100 to-gray-300/10"></div>
-        <p className=" w-full ">I am Sibi Sharanyan. I'm working remotely as a Full Stack Developer 🚀 for a US-based tech startup. I'm highly skilled with JavaScript/TypeScript 💻 and all its associated runtimes. I've worked for various startups in the past and have built applications from scratch with modern web technologies. I'm someone who is always on the lookout for modern tools that would improve the experience of both the developers and end-users. I'm a huge Productivity nerd who constantly tries to implement workflows and processes that would help me improve my productivity as a developer.</p>
+        <p className=" w-full ">I am Gaurav Kumar. I am a dedicated and innovative Frontend Developer with a passion for crafting engaging and user-friendly web experiences. With a strong background in HTML, CSS, Bootstrap, JavaScript, Tailwind CSS, React.js, and Redux, I specialize in developing dynamic dashboards, interactive applications, and visually appealing interfaces.
+        <br/><br/>Completed Bachelor of Computer Applications (BCA), focusing on algorithm design and web development. Committed to creating seamless, intuitive, and visually appealing digital solutions to enhance user engagement and satisfaction.</p>
+        <br/>Successfully developed and deployed various web applications, including:<br/>
+        <br/><strong>1. Teacher Review System:</strong> An online platform for students to provide feedback on their teachers' performance.
+        <br/><strong>2. GoAssignr:</strong> A task management system designed to streamline assignment and project tracking.
+        <br/><strong>3. ChatterRoom:</strong> A chat application using React.js and Node.js with Socket.IO, deployed on Vercel.
+        <br/><strong>4. CodeWithArt:</strong> A portfolio showcasing projects developed with React.js, Tailwind CSS, Framer Motion, and Redux.
+        
+        <br/><br/>Currently working on a video chat app enabling real-time interaction for up to four participants with integrated quiz features.
+        
       </div>
     )
   }
